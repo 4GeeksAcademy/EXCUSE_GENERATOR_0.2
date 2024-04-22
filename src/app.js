1dfc6b0bb5bc313b78484f81a1f790cee8576809
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-04-22 19:08:52 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").textContent = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["My brother", "A random person", "The mailman", "My friend"];
+  let action = ["ate", "peed", "stole", "broke"];
+  let what = ["my hat", "my phone", "my icecream", "my lunch"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "yesterday",
+    "this afternoon"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
